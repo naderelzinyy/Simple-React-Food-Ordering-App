f545cfa022cec720dce170fa28e73b46398aa3b7
naderelzinyy <naderelziny805@gmail.com>
2021-08-26 14:48:44 +0300

    Creating, styling and connecting MealItem.js (for each meal in MeatList)

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import Card from '../UI/Card'
 import classes from './AvailableMeals.module.css'
+import MealItem from './MealItem/MealItem'
+
 //
 //Dummy data for meals
 //
@@ -35,7 +37,12 @@ const DUMMY_MEALS = [
   
   export const AvailableMeals = () => {
 
-      const mealList = DUMMY_MEALS.map(meal=> <li> {meal.name} </li>)
+      const mealList = DUMMY_MEALS.map(meal=> <MealItem
+         key={meal.id}
+         name={meal.name}
+         description={meal.description}
+         price={meal.price}
+          /> )
 
       return (
           <section className={classes.meals}>
